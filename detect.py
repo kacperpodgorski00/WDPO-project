46ce2c772fe8ec2bc4fc7510a7e84270ea8f5544
Kacper Podgorski <kacperpodgorski00@gmail.com>
2022-12-02 13:55:28 +0100

    Adding resizing of images depending on their size.

--- detect.py
+++ detect.py
@@ -6,7 +6,6 @@ import click
 import cv2
 from tqdm import tqdm
 
-
 def detect(img_path: str) -> Dict[str, int]:
     """Object detection function, according to the project description, to implement.
 
@@ -21,6 +20,22 @@ def detect(img_path: str) -> Dict[str, int]:
         Dictionary with quantity of each object.
     """
     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+
+    # Resizing the image depending on its size
+    if ((img.shape[0] < img.shape[1]) or (img.shape[0] == img.shape[1])):
+        heightScale = 768 / img.shape[0]
+        widthScale = 1020 / img.shape[1]
+        resizedImg = cv2.resize(img, [int(img.shape[1] * widthScale), int(img.shape[0] * heightScale)], cv2.INTER_AREA)
+    elif ((img.shape[0] > img.shape[1]) or (img.shape[0] == img.shape[1])):
+        heightScale = 768 / img.shape[0]
+        widthScale = 1020 / img.shape[1]
+        resizedImg = cv2.resize(img, [int(img.shape[0] * heightScale), int(img.shape[1] * widthScale)], cv2.INTER_AREA)
+
+    cv2.imshow('window', resizedImg)
+
+    cv2.waitKey(0)
+
+    cv2.destroyAllWindows()
 
     #TODO: Implement detection method.
     
